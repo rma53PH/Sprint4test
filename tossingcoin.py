da7fe77004aec2df283947ed169aa87cec3901c4
rma53PH <robalicea12@outlook.com>
2024-09-26 00:10:15 +0800

    slider update

--- tossingcoin.py
+++ tossingcoin.py
@@ -2,12 +2,6 @@ import streamlit as st
 
 st.header('Tossing a Coin')
 
-st.write('It is not a functional application yet. Under construction.')
-
-import streamlit as st
-
-st.header('Tossing a Coin')
-
 number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
